74118877102f226f9c4d6a58c3891ceca3f3ab76
gaegulgaegul <dla9721@gmail.com>
2024-09-24 00:44:43 +0900

    API 데이터 FutureBuilder를 통해 확인하기

--- app_toonflix/lib/screens/home_screen.dart
+++ app_toonflix/lib/screens/home_screen.dart
@@ -1,10 +1,15 @@
+import 'package:app_toonflix/models/webtoon_model.dart';
+import 'package:app_toonflix/services/api_service.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
+    print(webtoons);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -18,6 +23,15 @@ class HomeScreen extends StatelessWidget {
             fontWeight: FontWeight.w600
           ),
         ),
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Text("데이터 있어");
+          }
+          return Text("Loading...");
+        }
       ),
     );
   }
